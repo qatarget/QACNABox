7fa3f6763090310de83ec869a3896a3ab5923688
MarcelloTarget <marcello.medeiros@targesoftware.com.br>
2018-08-17 16:44:59 -0300

    commit
    .

--- src/test/java/qa/cnabox/tests/MenuComercialTest.java
+++ src/test/java/qa/cnabox/tests/MenuComercialTest.java
@@ -4,11 +4,14 @@ import qa.cnabox.core.BaseTest;
 import qa.cnabox.page.MenuComercialPage;
 import static qa.cnabox.core.DriverFactory.getDriver;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
+
 
 public class MenuComercialTest extends BaseTest {
 
@@ -101,8 +104,57 @@ public class MenuComercialTest extends BaseTest {
 		getDriver().findElement(By.xpath("/html//textarea[@id='Descricao']")).sendKeys("Teste");
 		Thread.sleep(2000);
 		getDriver().findElement(By.cssSelector(".form-group [type='button']:nth-of-type(2)")).click();
-		
 
 	}
+	
+	//************************************************************ Filtros ***********************************************************************//
 
+	@Test
+	public void DeveRealizarFiltroDeProspectPorNome() throws InterruptedException {
+		getDriver()
+				.findElement(By.xpath(
+						"//div[@id='menu-lateral']/ul[@class='nav-left-bar']/li[6]/a[@href='#']/span[.='Comercial']"))
+				.click();
+		getDriver().findElement(By.xpath(
+				"//div[@id='menu-lateral']/ul[@class='nav-left-bar']/li[6]/ul[@class='sub-menu']//span[.='Atendimento']"))
+				.click();
+		Thread.sleep(3000);
+		getDriver().findElement(By.cssSelector("#AtendimentoNome")).sendKeys("ALUNO TESTE");
+		getDriver().findElement(By.cssSelector("#btnAtendimentoFilter")).click();
+		Thread.sleep(3000);
+		Assert.assertEquals("ALUNO TESTE", getDriver().findElement(By.cssSelector("#AtendimentoNome")).getAttribute("value"));
+		
+	}
+	
+	@Test
+	public void DeveRealizarFiltroDeProspectPorEmail() throws InterruptedException {
+		getDriver()
+				.findElement(By.xpath(
+						"//div[@id='menu-lateral']/ul[@class='nav-left-bar']/li[6]/a[@href='#']/span[.='Comercial']"))
+				.click();
+		getDriver().findElement(By.xpath(
+				"//div[@id='menu-lateral']/ul[@class='nav-left-bar']/li[6]/ul[@class='sub-menu']//span[.='Atendimento']"))
+				.click();
+		Thread.sleep(3000);
+		getDriver().findElement(By.cssSelector("#AtendimentoEmail")).sendKeys("TESTE@TESTE.COM.BR");
+		getDriver().findElement(By.cssSelector("#btnAtendimentoFilter")).click();
+		Thread.sleep(3000);
+		Assert.assertEquals("TESTE@TESTE.COM.BR", getDriver().findElement(By.cssSelector("#AtendimentoEmail")).getAttribute("value"));
+	}
+	
+	@Test
+	public void DeveRealizarFiltroDeProspectPorTelefone() throws InterruptedException {
+		getDriver()
+				.findElement(By.xpath(
+						"//div[@id='menu-lateral']/ul[@class='nav-left-bar']/li[6]/a[@href='#']/span[.='Comercial']"))
+				.click();
+		getDriver().findElement(By.xpath(
+				"//div[@id='menu-lateral']/ul[@class='nav-left-bar']/li[6]/ul[@class='sub-menu']//span[.='Atendimento']"))
+				.click();
+		Thread.sleep(3000);
+		getDriver().findElement(By.cssSelector("#AtendimentoTelefone")).sendKeys("  1997499671 ");
+		getDriver().findElement(By.cssSelector("#btnAtendimentoFilter")).click();
+		Thread.sleep(3000);
+		Assert.assertEquals("(19) 9749-9671", getDriver().findElement(By.cssSelector("#AtendimentoTelefone")).getAttribute("value"));
+	}
 }
