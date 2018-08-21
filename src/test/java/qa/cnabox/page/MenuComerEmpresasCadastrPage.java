b2fa8288ef499cac5c49807126d852ca1d5b2a51
carolinasampognaTgT <41452195+carolinasampognaTgT@users.noreply.github.com>
2018-08-21 17:06:36 -0300

    Empresas
    Empresas

---
+++ src/test/java/qa/cnabox/page/MenuComerEmpresasCadastrPage.java
@@ -0,0 +1,44 @@
+package qa.cnabox.page;
+
+import qa.cnabox.core.BasePage;
+import static qa.cnabox.core.DriverFactory.getDriver;
+
+import org.openqa.selenium.By;
+
+
+
+public class MenuComerEmpresasCadastrPage extends BasePage {
+	
+	public void SetclicarMenuComercial () throws InterruptedException  {
+		
+		clicarBotaoBy(By.cssSelector(".nav-left-bar li:nth-of-type(6) .menu-item"));
+		Thread.sleep(1000);
+		
+	}
+	
+	public void SetClicarEmpresas () throws InterruptedException {
+		
+		clicarBotaoBy(By.linkText("Empresas"));
+		Thread.sleep(1000);
+		
+	}
+
+	
+	public void SetClicarNovaEmpresa () throws InterruptedException{
+		
+		clicarBotaoBy(By.cssSelector(".list-btn [data-toggle]"));
+		Thread.sleep(1000);
+	}
+
+
+	public void SetescreverNome () throws InterruptedException {
+	 
+		clicarBotaoBy(By.cssSelector("#PessoaEscola_Nome"));
+		Thread.sleep(1000);
+		escreveTexto(By.cssSelector("PessoaEscola_Nome"), geraEmpresaAleatorio());
+}
+
+
+
+
+}
