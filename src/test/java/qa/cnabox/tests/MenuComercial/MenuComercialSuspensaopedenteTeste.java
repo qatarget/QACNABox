78198e2ed07cb685f2448700cb64aadc18dbb3e9
carolinasampognaTgT <41452195+carolinasampognaTgT@users.noreply.github.com>
2018-08-28 17:21:08 -0300

    Suspensao

---
+++ src/test/java/qa/cnabox/tests/MenuComercial/MenuComercialSuspensaopedenteTeste.java
@@ -0,0 +1,25 @@
+package qa.cnabox.tests.MenuComercial;
+
+import org.junit.Test;
+
+import qa.cnabox.core.BaseTest;
+import qa.cnabox.page.MenuComercial.MenuComercialOrcamentoPage;
+
+public class MenuComercialSuspensaopedenteTeste extends BaseTest {
+
+	MenuComercialOrcamentoPage page = new MenuComercialOrcamentoPage();
+	
+	@Test
+
+	public void Deverealizarasuspensao () throws InterruptedException {
+
+
+	
+	
+	
+	
+	
+	}
+
+
+}
