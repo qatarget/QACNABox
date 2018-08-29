9a80f87c032323ec050c536a413ff05005ead275
carolinasampognaTgT <41452195+carolinasampognaTgT@users.noreply.github.com>
2018-08-29 12:18:58 -0300

    COmitty

--- src/test/java/qa/cnabox/page/MenuRelatorioComercial/MenuRelatorioComercialAtendimentoPage.java
+++ src/test/java/qa/cnabox/page/MenuRelatorioComercial/MenuRelatorioComercialAtendimentoPage.java
@@ -1,6 +1,9 @@
 package qa.cnabox.page.MenuRelatorioComercial;
 
+import static qa.cnabox.core.DriverFactory.getDriver;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 
 import qa.cnabox.core.BasePage;
 
@@ -29,10 +32,34 @@ public class MenuRelatorioComercialAtendimentoPage extends BasePage {
 		escreveTexto(By.cssSelector("#DataRealizacaoAtendimentoDe"), Texto);
 	}
 	
-public void SetSelecionarPeriodoAte(String Texto) throws InterruptedException{
+	public void SetSelecionarPeriodoAte(String Texto) throws InterruptedException{
 		
 		clicarBotaoBy(By.cssSelector("#DataRealizacaoAtendimentoAte"));
 		Thread.sleep(1000);
 		escreveTexto(By.cssSelector("#DataRealizacaoAtendimentoAte"), Texto);
 	}
+	
+	public void SetSituacaodoAtendido( String Texto) throws InterruptedException{
+	
+								////Legenda///
+		///** 3- ALUNO, 1-CADASTRO, 2-Prospect, 4-Empresa, 5-Ex-Aluno, 6-Suspenso///
+	
+	Select dropdown = new Select(getDriver().findElement(By.cssSelector(".select2-selection--multiple .select2-selection__rendered")));
+	dropdown.selectByValue(Texto);
+	Thread.sleep(1000);
+	
+	}
+	
+	public void SetTipoatendimento () throws InterruptedException{
+		
+		
+	}
+	
+	public void Setfiltrartendimento () throws InterruptedException{
+		
+		clicarBotaoBy(By.id("btnRelatorioAtendimentosFilter"));
+		Thread.sleep(1000);
+		
+	}
+	
 }
