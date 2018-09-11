0b220e1247677e9b8aff297ac2d8e987bee203ce
MarcelloTarget <marcello.medeiros@targesoftware.com.br>
2018-09-11 18:05:11 -0300

    commit
    .

--- src/test/java/Page/MenuPedagógico/MenuPedagogicoFiltroAlunoPage.java
+++ src/test/java/Page/MenuPedagógico/MenuPedagogicoFiltroAlunoPage.java
@@ -4,28 +4,32 @@ import qa.cnabox.core.BasePage;
 import static qa.cnabox.core.DriverFactory.getDriver;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
 
-public class MenuPedagogicoFiltroAlunoPage  extends BasePage {
+
+public class MenuPedagogicoFiltroAlunoPage extends BasePage {
 
 	public void SetClicarMenuPedagógico() {
 
 		getDriver().findElement(By.cssSelector(".nav-left-bar li:nth-of-type(8) .menu-item span")).click();
 
 	}
-	
+
 	public void SetClicarAlunos() throws InterruptedException {
-		
+
 		getDriver().findElement(By.cssSelector(".nav-left-bar li:nth-of-type(8) li:nth-of-type(1) span")).click();
 		Thread.sleep(3000);
 	}
-	
+
 	public void SetNomeAluno(String nomeAluno) {
-		
+
 		escreveTexto(By.cssSelector("#Nome"), nomeAluno);
-				
+
+	}
+
+	public void ClicarBotãoFIltrar() throws InterruptedException {
+
+		getDriver().findElement(By.cssSelector("#order-form footer [type='button']:nth-of-type(1)")).click();
+		Thread.sleep(3000);
 	}
 
 }
