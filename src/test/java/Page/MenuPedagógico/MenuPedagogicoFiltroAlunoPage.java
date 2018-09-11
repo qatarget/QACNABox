1d7f57ebb4bd4a04d2e5cbe280e4245115aea5c7
MarcelloTarget <marcello.medeiros@targesoftware.com.br>
2018-09-11 18:01:40 -0300

    Commit
    Criação de Page Pedagógico (FIltro aluno)

---
+++ src/test/java/Page/MenuPedagógico/MenuPedagogicoFiltroAlunoPage.java
@@ -0,0 +1,31 @@
+package Page.MenuPedagógico;
+
+import qa.cnabox.core.BasePage;
+import static qa.cnabox.core.DriverFactory.getDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class MenuPedagogicoFiltroAlunoPage  extends BasePage {
+
+	public void SetClicarMenuPedagógico() {
+
+		getDriver().findElement(By.cssSelector(".nav-left-bar li:nth-of-type(8) .menu-item span")).click();
+
+	}
+	
+	public void SetClicarAlunos() throws InterruptedException {
+		
+		getDriver().findElement(By.cssSelector(".nav-left-bar li:nth-of-type(8) li:nth-of-type(1) span")).click();
+		Thread.sleep(3000);
+	}
+	
+	public void SetNomeAluno(String nomeAluno) {
+		
+		escreveTexto(By.cssSelector("#Nome"), nomeAluno);
+				
+	}
+
+}
